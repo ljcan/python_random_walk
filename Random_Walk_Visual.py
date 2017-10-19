1cf48e853c996d6d673df63f290dace6341ec6a7
ljcan <31683893+ljcan@users.noreply.github.com>
2017-10-19 17:49:44 +0800

    Update Random_Walk_Visual.py

--- Random_Walk_Visual.py
+++ Random_Walk_Visual.py
@@ -5,6 +5,7 @@ while True:
   RW.walk()
   #使颜色渐变
   point_number=list(range(RW.number_point))
+  # the parameter of 's' is set the size of point
   plt.scatter(RW.x_values,RW.y_values,c=point_number,cmap=plt.cm.Blues,edgecolors='None',s=15)
   plt.show()
   keep_running=raw_input('Make another walk?(Y/N):')
